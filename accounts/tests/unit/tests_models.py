b58de48c596d863d91c99dc4d46f7b9d31d7b7c1
tashi.cse <tashi.cse@gmail.com>
2023-10-22 17:38:45 +0200

    update test

--- accounts/tests/unit/tests_models.py
+++ accounts/tests/unit/tests_models.py
@@ -1,19 +1,27 @@
 from django.test import TestCase, Client
 from accounts.models import User
 from django.test import RequestFactory
+from django.contrib.auth import get_user_model
 
-
-class TestModels(TestCase):
-    def setUp(self):
-        self.factory = RequestFactory()
-        self.user = User.objects.create_user(
-            username="inconnu",
-            email="inconnu@email.com",
-            password="1234AZERTY",
-            first_name="john",
-            last_name="snow",
-
+class CustomUserTests(TestCase):
+    def test_create_user(self): 
+        User = get_user_model()
+        user = User.objects.create_user(
+            username="will", email="will@email.com", password="testpass123",
+            first_name="Willy", last_name="Snow"
         )
+        self.assertEqual(user.username, "will")
+        self.assertEqual(user.email, "will@email.com")
 
-    def test_user_str(self):
-        self.assertEqual(str(self.user), "inconnu@email.com")
+    def test_create_superuser(self):
+        User = get_user_model()   
+        admin_user = User.objects.create_superuser(
+            username="superadmin", email="superadmin@email.com", password="testpass123",
+            first_name="john", last_name="thomas"
+        ) 
+        self.assertEqual(admin_user.username, "superadmin")
+        self.assertEqual(admin_user.email, "superadmin@email.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superadmin)
+
