f89e8b239989d7a9f15aba6adb6714bb9a1778e7
tashi.cse <tashi.cse@gmail.com>
2023-10-22 22:30:52 +0200

    update custome user test

--- accounts/tests/unit/tests_models.py
+++ accounts/tests/unit/tests_models.py
@@ -24,6 +24,29 @@ class CustomUserTests(TestCase):
         self.assertTrue(admin_user.is_active)
         self.assertTrue(admin_user.is_staff)
         self.assertTrue(admin_user.is_superadmin)
-        self.assertIs(admin_user.has_perm("fake permission"), True)            
+        self.assertIs(admin_user.has_perm("fake permission"), True)     
 
+
+    def test_customer_chef_role(self):
+        self.customer_user = User.objects.create(
+            email='customer@example.com',
+            username='customer_user',
+            first_name='Alice',
+            last_name='Johnson',
+            role=User.CUSTOMER
+        )       
+
+        customer_role = self.customer_user.get_role()
+        self.assertEqual(customer_role, 'Customer')
+
+        self.chef = User.objects.create(
+            email='chef@example.com',
+            username='chef_user',
+            first_name='John',
+            last_name='Doe',
+            role=User.CHEF
+        )
+
+        chef_role = self.chef.get_role()
+        self.assertEqual(chef_role,'Chef')
 
