f85a0dfc4c37e720fb6af370046b0b218e8f097c
tashi.cse <tashi.cse@gmail.com>
2023-10-29 22:45:46 +0100

    update view

--- accounts/views.py
+++ accounts/views.py
@@ -12,15 +12,9 @@ class RegisterUserView(CreateView):
     def post(self, request):
         form = self.form_class(request.POST)
         if form.is_valid():
-            # Create the user using create_user method
-            first_name = form.cleaned_data['first_name']
-            last_name = form.cleaned_data['last_name']
-            username = form.cleaned_data['username']
-            email = form.cleaned_data['email']
-            password = form.cleaned_data['password']
-            user = User.objects.create_user(first_name=first_name, last_name=last_name, 
-                                               username=username, email=email, 
-                                               password=password)
+            # Create the user using the form
+            user = form.save(commit=False)
+            user.set_password(form.cleaned_data['password'])
             user.role = User.CUSTOMER
             user.save()
             messages.success(request, 'Your account has been registered sucessfully!')
@@ -30,5 +24,3 @@ class RegisterUserView(CreateView):
             print(form.errors)
         return render(request, self.template_name, context={'form': form})
 
-# def registerUser(request):
-#     return  HttpResponse("This is a user registeration form")
