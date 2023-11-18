d752898c9e45338021f9659830fa4443819c66d5
tashi.cse <tashi.cse@gmail.com>
2023-11-19 00:45:14 +0100

    add chef and customer views

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
-from .views import RegisterUserView, RegisterChefView
+from . import views
+
 
 urlpatterns = [
-    path("registerUser/", RegisterUserView.as_view(), name="registerUser"),
-    path("registerChef/", RegisterChefView.as_view(), name="registerChef"),
+    path("registerUser/", views.RegisterUserView.as_view(), name="registerUser"),
+    path("registerChef/", views.RegisterChefView.as_view(), name="registerChef"),
+    path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path(
+        "customer/dashboard/",
+        views.CustomerDashboardView.as_view(),
+        name="customerDashboard",
+    ),
+    path(
+        "chef/dashboard/",
+        views.ChefDashboardView.as_view(),
+        name="chefDashboard",
+    ),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import redirect
+from django.shortcuts import redirect, render
 
 from django.views.generic import CreateView
 from .forms import UserForm
-from django.contrib import messages
-from django.contrib.auth import login
 from chef.forms import ChefForm
 from .models import User, UserProfile
+from django.contrib import messages, auth
+from django.contrib.auth import login
+from django.contrib.auth import authenticate, login
+from django.shortcuts import redirect, render
+from django.views import View
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import UserPassesTestMixin
+from django.views.generic import TemplateView
 
 
-# Create your views here.
 class RegisterUserView(CreateView):
     form_class = UserForm
     template_name = "accounts/registerUser.html"
@@ -60,5 +66,70 @@ class RegisterChefView(CreateView):
         return context
 
 
-# def registerChef(request):
-#    return render(request, "accounts/registerChef.html")
+class LoginView(View):
+    template_name = "accounts/login.html"
+    success_customer_url = reverse_lazy("customerDashboard")
+    success_chef_url = reverse_lazy("chefDashboard")
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.warning(request, "You are already logged in!")
+            return self._redirect_to_dashboard(request.user.role)
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.warning(request, "You are already logged in!")
+            return self._redirect_to_dashboard(request.user.role)
+
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = authenticate(request, email=email, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "You are now logged in.")
+            return self._redirect_to_dashboard(user.role)
+        else:
+            messages.error(request, "Invalid login credentials")
+            return redirect("login")
+
+    def _redirect_to_dashboard(self, role):
+        if role == 1:  # Chef role
+            return redirect(self.success_chef_url)
+        elif role == 2:  # Customer role
+            return redirect(self.success_customer_url)
+
+
+class LogoutView(TemplateView):
+    def get(self, request):
+        auth.logout(request)
+        messages.info(request, "You are logged out.")
+        return redirect("login")
+
+
+class CustomerDashboardView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = "accounts/Customerdashboard.html"
+    login_url = reverse_lazy("login")
+
+    def test_func(self):
+        return self.request.user.role == 2
+
+    def handle_no_permission(self):
+        if self.request.user.role == 1:
+            return redirect("chefDashboard")
+        return super().handle_no_permission()
+
+
+class ChefDashboardView(LoginRequiredMixin, UserPassesTestMixin, TemplateView):
+    template_name = "accounts/Chefdashboard.html"
+    login_url = reverse_lazy("login")
+
+    def test_func(self):
+        return self.request.user.role == 1
+
+    def handle_no_permission(self):
+        if self.request.user.role == 2:
+            return redirect("customerDashboard")
+        return super().handle_no_permission()
