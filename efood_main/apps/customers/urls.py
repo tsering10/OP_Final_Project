5fc3d96ab359de9257d5b3a87933b22fd0888071
tashi.cse <tashi.cse@gmail.com>
2024-02-16 18:38:25 +0900

    add customer booked workhop url

--- efood_main/apps/customers/urls.py
+++ efood_main/apps/customers/urls.py
@@ -14,7 +14,7 @@ urlpatterns = [
     path("c-profile/", views.CustomerProfileView.as_view(), name="cust_profile"),
     path(
         "workshop/",
-        views.CustomerWorkshopsListView.as_view(),
+        views.UserBookedWorkshopsView.as_view(),
         name="customer_workshop",
     ),
     path(
