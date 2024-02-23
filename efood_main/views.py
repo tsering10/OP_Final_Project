e9b41f613f3eaa3776111f095bec70998f2635a7
tashi.cse <tashi.cse@gmail.com>
2024-02-23 17:39:22 +0900

    add search view

--- efood_main/views.py
+++ efood_main/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Q
 from django.views.generic import ListView
 
 from efood_main.apps.recipe.models import RecipeItem
@@ -8,3 +9,20 @@ class HomePageView(ListView):
     template_name = "home.html"
     context_object_name = "recipe_items"
     queryset = RecipeItem.objects.all().order_by("created_at")[:4]
+
+
+class RecipeSearchListView(ListView):
+    model = RecipeItem
+    template_name = "recipe_search_results.html"
+    context_object_name = "search_recipes"
+
+    def get_queryset(self):
+        query = self.request.GET.get("q")
+        if query:
+            return RecipeItem.objects.filter(
+                Q(recipe_title__icontains=query)
+                | Q(recipe_ingredients__icontains=query)
+                | Q(category__category_name__icontains=query)
+            ).distinct()
+        else:
+            return RecipeItem.objects.none()
