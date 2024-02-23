09c9162a82576487bca8878f947b19f0d2288d38
tashi.cse <tashi.cse@gmail.com>
2024-02-23 17:39:13 +0900

    add url for search

--- efood_main/urls.py
+++ efood_main/urls.py
@@ -14,15 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import include, path
+
 from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.HomePageView.as_view(), name="home"),
+    path("search/", views.RecipeSearchListView.as_view(), name="recipe_search"),
     path("accounts/", include("efood_main.apps.accounts.urls")),
     # path("", include("efood_main.apps.accounts.urls")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
