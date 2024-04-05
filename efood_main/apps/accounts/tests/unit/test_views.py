660c119cee476b90672bdc4100950a87a73ae250
tashi.cse <tashi.cse@gmail.com>
2024-04-05 23:37:22 +0200

    add test for customer

--- efood_main/apps/accounts/tests/unit/test_views.py
+++ efood_main/apps/accounts/tests/unit/test_views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.tokens import default_token_generator
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.test import Client, TestCase
 from django.urls import reverse
 from django.utils.encoding import force_bytes
 from django.utils.http import urlsafe_base64_encode
 
-from efood_main.apps.accounts.models import User
+from efood_main.apps.accounts.models import User, UserProfile
+from efood_main.apps.chef.models import Chef
 
 
 class TestRegisterUserView(TestCase):
@@ -103,3 +105,82 @@ class ActivateViewTests(TestCase):
         self.user.refresh_from_db()
         self.assertTrue(self.user.is_active)
         self.assertRedirects(response, reverse("login"))
+
+    def test_activate_invalid(self):
+        url = reverse("activate", args=["bad-uid", "bad-token"])
+        response = self.client.get(url)
+        self.assertRedirects(response, reverse("login"), fetch_redirect_response=False)
+
+
+class RegisterUserViewTestCase(TestCase):
+    def setUp(self):
+        # Setup request data for user registration
+        self.user_data = {
+            "first_name": "Test",
+            "last_name": "User",
+            "username": "testuser",
+            "email": "test@example.com",
+            "password": "password1",
+            "confirm_password": "password2",
+        }
+
+    def test_register_user_form_valid(self):
+        response = self.client.post(reverse("registerUser"), self.user_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "first_name")
+        self.assertTemplateUsed(response, "accounts/registerUser.html")
+
+    def test_register_user_form_invalid(self):
+        # Modify one field to make the form invalid
+        invalid_data = self.user_data.copy()
+        invalid_data["email"] = ""  # Assuming email is required
+        response = self.client.post(reverse("registerUser"), invalid_data)
+        self.assertEqual(response.status_code, 200)
+        self.assertFalse(User.objects.filter(email="").exists())
+
+
+class RegisterChefViewTestCase(TestCase):
+    def setUp(self):
+        # Setup request data for chef registration
+        self.chef_data = {
+            "first_name": "Test",
+            "last_name": "User",
+            "username": "testuser",
+            "email": "test@example.com",
+            "password": "password1",
+            "confirm_password": "password2",
+            "chef_name": "Test Chef",
+        }
+
+    def test_register_chef_form_valid(self):
+        response = self.client.post(reverse("registerChef"), self.chef_data)
+        self.assertContains(response, "chef_name")
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_chef_form_invalid(self):
+        # Modify one field to make the form invalid
+        invalid_data = self.chef_data.copy()
+        invalid_data["email"] = ""  # Assuming email is required
+        response = self.client.post(reverse("registerChef"), invalid_data)
+        self.assertEqual(response.status_code, 200)  # Page reloads with form errors
+        self.assertFalse(User.objects.filter(email="").exists())
+
+
+class CustDashboardViewTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            username="testuser",
+            first_name="john",
+            last_name="doe",
+            email="testuser@example.com",
+            password="test1234",
+            is_active=True,
+            role=2,
+        )
+        self.profile, _ = UserProfile.objects.get_or_create(user=self.user)
+        self.client.force_login(self.user)
+
+    def test_customer_dashboard_view(self):
+        response = self.client.get(reverse("customerDashboard"))
+        self.assertTrue("workshops" in response.context)
+        self.assertEqual(response.status_code, 200)
--- efood_main/apps/customers/tests.py
+++ efood_main/apps/customers/tests.py
@@ -1,10 +1,16 @@
+from datetime import date, time
+
+from django.contrib.messages import get_messages
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import reverse
 
 from efood_main.apps.accounts.forms import UserInfoForm, UserProfileForm
 from efood_main.apps.accounts.models import User, UserProfile
+from efood_main.apps.chef.models import Chef
 from efood_main.apps.customers.views import CustomerViewMixin
+from efood_main.apps.workshop.models import Workshop
 
 
 class CustomerViewMixinTest(TestCase):
@@ -25,27 +31,20 @@ class CustomerViewMixinTest(TestCase):
         # Attach the is_customer attribute to simulate a customer
         self.user.role = User.CUSTOMER
         self.user.save()
-
         request = HttpRequest()
         request.user = self.user
-
         mixin = CustomerViewMixin()
         mixin.request = request
-
-        # Test the customer case
         self.assertTrue(mixin.test_func(), "test_func should return True for customers")
 
     def test_non_customer_user(self):
         # Set the user's role to CHEF
         self.user.role = User.CHEF
         self.user.save()
-
         request = HttpRequest()
         request.user = self.user
-
         mixin = CustomerViewMixin()
         mixin.request = request
-
         # Test the non-customer case
         self.assertFalse(
             mixin.test_func(), "test_func should return False for non-customers"
@@ -81,6 +80,87 @@ class CustomerProfileViewTest(TestCase):
         self.assertIsInstance(
             response.context["combined_form"]["user_form"], UserInfoForm
         )
+        self.assertIn("combined_form", response.context_data)
+
+    def test_post_with_valid_data_triggers_form_valid(self):
+        # Prepare valid data for your UserProfileForm and UserInfoForm
+        profile_picture = SimpleUploadedFile(
+            "profile_pic.jpg", b"file_content", content_type="image/jpeg"
+        )
+        cover_photo = SimpleUploadedFile(
+            "cover_photo.jpg", b"file_content", content_type="image/jpeg"
+        )
+        valid_data = {
+            "first_name": "Test",
+            "last_name": "User",
+            "username": "testuser",
+            "email": "testupdate@example.com",
+            "phone_number": "1234567890",
+            "profile_picture": profile_picture,
+            "cover_photo": cover_photo,
+            "address": "123 Main St",
+            "city": "Anytown",
+            "country": "Country",
+            "state": "State",
+            "postal_code": "12345",
+        }
+        response = self.client.post(reverse("cust_profile"), data=valid_data)
+
+        self.assertRedirects(response, reverse("customer"))
+        messages = list(get_messages(response.wsgi_request))
+        self.assertIn("Profile updated.", [message.message for message in messages])
+        self.user.refresh_from_db()
+        self.assertEqual(self.user.first_name, "Test")
+        self.assertEqual(self.user.last_name, "User")
+
+
+class CustomerWorkshopDetailViewTest(TestCase):
+    def setUp(self):
+        self.chef = User.objects.create_user(
+            username="chefuser",
+            email="chef@example.com",
+            password="chefpassword",
+            first_name="Test",
+            last_name="Chef",
+        )
+
+        # Create a user and profile
+        self.customer = User.objects.create(
+            username="testuser",
+            first_name="henry",
+            last_name="doe",
+            email="testuser@example.com",
+            password="test12345",
+            is_active=True,
+            role=2,
+        )
+        self.cust_profile, _ = UserProfile.objects.get_or_create(user=self.customer)
+        self.chef_profile, _ = UserProfile.objects.get_or_create(user=self.chef)
+        Chef.objects.create(
+            user=self.chef, user_profile=self.chef_profile, chef_name="TestChef"
+        )
+        chef = Chef.objects.get(user=self.chef)
+
+        self.workshop = Workshop.objects.create(
+            chef=chef,
+            title="Test Workshop",
+            description="This is a test workshop.",
+            date=date.today(),
+            time=time(14, 30),
+            capacity=10,
+            price=100.00,
+        )
+
+        self.client.force_login(self.customer)
+
+    def test_view_renders_correct_workshop_for_customer(self):
+        url = reverse("cust-workshop-detail", kwargs={"id": self.workshop.id})
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context["workshop"].id, self.workshop.id)
+        self.assertEqual(response.context["workshop"].title, "Test Workshop")
+        self.assertContains(response, "Test Workshop")
+        self.assertContains(response, "This is a test workshop.")
 
 
 class CustomerWorkshopBookTest(TestCase):
